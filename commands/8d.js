ecca30799e560cf7b838bff616dfdfcefb123d1a
Akshit Gupta <akshitthecoder@gmail.com>
2021-01-28 18:08:31 +0530

    fixed one error

--- commands/8d.js
+++ commands/8d.js
@@ -10,7 +10,8 @@ module.exports = {
     run: async function (client, message, args) {
         if (!message.member.voice.channel) return message.channel.send("❌ | You must be in a voice channel to use this command!");
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send("❌ | You are not in my voice channel!");
-        if (!client.player.isPlaying(message.guild.id)){
+        const serverQueue = message.client.queue.get(message.guild.id);
+        if (!serverQueue && !serverQueue.playing){
             return message.channel.send("❌ | I'm not playing anything?");
         }
         const edEnabled = await client.player.getQueue(message.guild.id).filters["8D"];
