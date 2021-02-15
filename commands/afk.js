5537104f168780f8ac50a9ce411a0009ac88ff4b
repl.it user <replituser@example.com>
2021-02-15 07:41:40 +0000

    modified some files

--- commands/afk.js
+++ commands/afk.js
@@ -13,26 +13,24 @@ module.exports = {
 
   run: async function (client, message, args) {
     let afk = JSON.parse(fs.readFileSync("./afk.json", "utf8"));
-    if(message.member.hasPermission('MANAGE_SERVER')){
-      if (!afk[message.guild.id]) afk[message.guild.id] = {
-        afk: false,
-      };
-      var serverQueue = afk[message.guild.id]
-      if (serverQueue) {
+    if(!message.member.hasPermission('MANAGE_SERVER'))return sendError("You do not have permission to use this command", message.channel);
+    if (!afk[message.guild.id]) afk[message.guild.id] = {
+      afk: false,
+    };
+    var serverQueue = afk[message.guild.id]
+    if (serverQueue) {
 
-        serverQueue.afk = !serverQueue.afk;
-        message.channel.send({
-          embed: {
-            color: "GREEN",
-            description: `💤  **|**  AFK is **\`${serverQueue.afk === true ? "enabled" : "disabled"}\`**`
-          }
-        });
-        return  fs.writeFile("./afk.json", JSON.stringify(afk), (err) => {
-          if (err) console.error(err);
-        });
-      };
-      return sendError("There is nothing playing in this server.", message.channel);
-    }
-    return sendError("You do not have permission to use this command", message.channel);
+      serverQueue.afk = !serverQueue.afk;
+      message.channel.send({
+        embed: {
+          color: "GREEN",
+          description: `💤  **|**  AFK is **\`${serverQueue.afk === true ? "enabled" : "disabled"}\`**`
+        }
+      });
+      return  fs.writeFile("./afk.json", JSON.stringify(afk), (err) => {
+        if (err) console.error(err);
+      });
+    };
+    return sendError("There is nothing playing in this server.", message.channel);
   },
 };
--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,15 @@
 module.exports = async (client) => {
-    console.log(`[API] Logged in as ${client.user.username}`);
-    await client.user.setActivity("Music and mhelp", {
-      type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+  console.log(`[API] Logged in as ${client.user.username}`);
+
+  let statuses = [
+    "mhelp",
+    "Music"
+  ]
+
+  setInterval(function() {
+    let status = statuses[Math.floor(Math.random() * statuses.length)];
+    client.user.setActivity(status, {
+      type: "LISTENING",
     });
-  };
+  }, 1000);
+};
